ec883117490c7827895b153f67762e1c59980ea8
Odorsaf <dothmen@dom.tti>
2023-11-07 10:54:04 +0100

    mess

--- src/test/java/teststeps/Runner.java
+++ src/test/java/teststeps/Runner.java
@@ -12,7 +12,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 
 @CucumberOptions(features="src\\test\\resources\\Features" , glue={"teststeps"} , strict = true, tags="@update", plugin = {
-		"pretty", "html:target/rapport.html", "json:target/rapport.json"
+		"pretty", "html:target/rapport.html","json:target/rapport.json"
 } )
 
 
